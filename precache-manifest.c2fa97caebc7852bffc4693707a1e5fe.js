831d5b39a17de15f3fffa08c4e7bcd0ecd0f4d97
Yury Firsau <janin90@yandex.ru>
2020-08-18 18:41:50 +0300

    Updates

---
+++ precache-manifest.c2fa97caebc7852bffc4693707a1e5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fab6580dd584c02487c20493b7337ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4786237901d64859aa7a",
+    "url": "/static/css/main.e2b73990.chunk.css"
+  },
+  {
+    "revision": "06d2f427e43d9ae89d1f",
+    "url": "/static/js/2.06da3e5d.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.06da3e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4786237901d64859aa7a",
+    "url": "/static/js/main.1594834a.chunk.js"
+  },
+  {
+    "revision": "1733ad7b859d58d9e8d8",
+    "url": "/static/js/runtime-main.3bff6ce9.js"
+  }
+]);
